3a1fa6cee58211e568bcba4b413d81b5a33f467d
Goce Mitevski <goce.mitevski@gmail.com>
2025-12-28 21:12:59 +0100

    Add total counter cards

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -2,6 +2,7 @@ import { Fragment, useEffect, useMemo, useState } from "react";
 import SearchForm from "./SearchForm";
 import { filterDefinitions } from "../utils/filterDefinitions";
 import Card from "./Card";
+import { parseDecimalNumber, sumDecimalNumbers } from "../utils/decimalNumbers";
 
 export default function Cards({ tableData, money }) {
   const [filters, setFilters] = useState(filterDefinitions);
@@ -40,8 +41,53 @@ export default function Cards({ tableData, money }) {
     setFilteredData(tableData);
   }, [tableData]);
 
+  let totalCompanies = [...new Set(money.map((item) => item.Назив))].length;
+  let totalIncome = parseDecimalNumber(
+    sumDecimalNumbers(money.map((item) => item.Приходи))
+  );
+  let totalOutcome = parseDecimalNumber(
+    sumDecimalNumbers(money.map((item) => item.Расходи))
+  );
+  let totalFinancialResults = parseDecimalNumber(
+    sumDecimalNumbers(money.map((item) => item[`Финансиски резултат`]))
+  );
+
   return (
     <Fragment>
+      <div className="row row-cols-1 row-cols-lg-4 g-3 mt-2 mb-3">
+        <div className="col">
+          <div className="card">
+            <div className="card-body">
+              <h5>{totalCompanies}</h5>
+              <p className="card-text">Јавни претпријатија и трговски друштва</p>
+            </div>
+          </div>
+        </div>
+        <div className="col">
+          <div className="card">
+            <div className="card-body">
+              <h5>{totalIncome}</h5>
+              <p className="card-text">Приходи за избраната година и квартал</p>
+            </div>
+          </div>
+        </div>
+        <div className="col">
+          <div className="card">
+            <div className="card-body">
+              <h5>{totalOutcome}</h5>
+              <p className="card-text">Расходи за избраната година и квартал</p>
+            </div>
+          </div>
+        </div>
+        <div className="col">
+          <div className="card">
+            <div className="card-body">
+              <h5>{totalFinancialResults}</h5>
+              <p className="card-text">Финансиски резултат за избраната година и квартал</p>
+            </div>
+          </div>
+        </div>
+      </div>
       <SearchForm
         value={filters[Object.keys(filters)[7]]}
         index={Object.keys(filters)[7]}
@@ -51,12 +97,10 @@ export default function Cards({ tableData, money }) {
       />
       <div className="row row-cols-1 g-3 mb-4">
         {results.map((row, i) => (
-          <div className="col" key={i}>
+          row && <div className="col" key={i}>
             <Card
               row={row}
-              numbers={money.filter(
-                (el) => el.Назив == row.Назив
-              )}
+              numbers={money.filter((el) => el.Назив === row.Назив)}
             />
           </div>
         ))}
