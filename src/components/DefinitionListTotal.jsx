50e8ab660c42de43e47c901ea43abf368494c5ac
Goce Mitevski <goce.mitevski@gmail.com>
2026-03-05 19:08:19 +0100

    Update padding CSS class in DefinitionListTotal

--- src/components/DefinitionListTotal.jsx
+++ src/components/DefinitionListTotal.jsx
@@ -1,7 +1,7 @@
 export default function DefinitionListTotal({ title, total, icon, color, isActive }) {
 
   return (
-    <dl className={`hstack gap-2 mb-0 p-2 rounded ${isActive ? "bg-warning-subtle" : ""}`}>
+    <dl className={`hstack gap-2 mb-0 px-2 rounded ${isActive ? "bg-warning-subtle" : ""}`}>
       <dt className="hstack gap-2 mb-0">
         <i className={`bi ${icon} text-${color}`}></i>
         <span>{title}</span>
