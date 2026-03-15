157c1da5dc8160dd39709c195edea5e38744e954
Goce Mitevski <goce.mitevski@gmail.com>
2026-03-15 09:35:50 +0100

    Add bg-shade CSS class to SummaryCards

--- src/components/SummaryCards.jsx
+++ src/components/SummaryCards.jsx
@@ -141,7 +141,7 @@ function SummaryCards({ money, selectedYear, selectedQuarter }) {
   ];
 
   return (
-    <div className="bg-primary-subtle pt-5">
+    <div className="bg-primary-subtle bg-shade pt-5">
       <div className="container">
         <h1 className="fw-light m-3 text-secondary">
           {quarter > 0
