23adc196de1a23ddb341bb98d593271c291c792e
Goce Mitevski <goce.mitevski@gmail.com>
2026-03-16 11:25:09 +0100

    Set showCards prop for Cards

--- src/components/Registry.jsx
+++ src/components/Registry.jsx
@@ -150,6 +150,7 @@ function Registry() {
         activeSort={selectedSorting}
         selectedYear={selectedYear}
         selectedQuarter={selectedQuarter}
+        showChart={false}
       />
     </>
   );
