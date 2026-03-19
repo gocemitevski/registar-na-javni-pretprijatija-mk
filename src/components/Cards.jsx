32b9d524db0ebcd1269b1a5bddad9f46922936c3
Goce Mitevski <goce.mitevski@gmail.com>
2026-03-19 14:43:35 +0100

    Add and use CardsTotals component

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,11 +1,11 @@
-import { Fragment, useMemo, useState, useCallback } from "react";
+import { useMemo, useState, useCallback } from "react";
 import { useTranslation } from "react-i18next";
 import SearchForm from "./SearchForm";
 import { filterDefinitions } from "../utils/filterDefinitions";
 import Card from "./Card";
 import NoResults from "./NoResults";
-import { parseDecimalNumber, sumDecimalNumbers } from "../utils/decimalNumbers";
-import DefinitionList from "./DefinitionList";
+import { sumDecimalNumbers } from "../utils/decimalNumbers";
+import CardsTotals from "./CardsTotals";
 import FilteredChart from "./FilteredChart";
 
 export default function Cards({
@@ -18,7 +18,7 @@ export default function Cards({
   showSearch = true,
   filter,
 }) {
-  const { t, i18n } = useTranslation();
+  const { i18n } = useTranslation();
   const lang = i18n.language || "mk";
   const [filters, setFilters] = useState(filterDefinitions);
 
@@ -150,51 +150,15 @@ export default function Cards({
           )}
         </div>
       </div>
-      <div className="sticky-bottom py-4">
-        <div className="container bg-total totals bg-opacity-25 backdrop-blur border border-light shadow-lg py-5 rounded">
-          <div className="row mx-2">
-            <div className="col-lg-6 col-xl-8 vstack gap-2 justify-content-center">
-              <h5 className="card-title">
-                {selectedQuarter > 0
-                  ? t("cards.totalQuarter", {
-                      year: selectedYear,
-                      quarter: selectedQuarter,
-                    })
-                  : t("cards.total", { year: selectedYear })}
-              </h5>
-              <p className="card-text mb-4 mb-lg-0">
-                {totalCompanies}{" "}
-                {totalCompanies % 10 === 1 && totalCompanies !== 11
-                  ? t("cards.company_singular")
-                  : t("cards.company_plural")}
-              </p>
-            </div>
-            <div className="col-lg-6 col-xl-4 align-self-center vstack gap-2 ps-xl-0">
-              <DefinitionList
-                title={t("cards.income")}
-                total={parseDecimalNumber(totals.income, lang)}
-                rawValue={totals.income}
-                icon="bi-arrow-down"
-                isActive={activeSort === "income"}
-              />
-              <DefinitionList
-                title={t("cards.expenses")}
-                total={parseDecimalNumber(totals.expenses, lang)}
-                rawValue={totals.expenses}
-                icon="bi-arrow-up"
-                isActive={activeSort === "expenses"}
-              />
-              <DefinitionList
-                title={t("cards.financial-result")}
-                total={parseDecimalNumber(totals["financial-result"], lang)}
-                rawValue={totals["financial-result"]}
-                icon="bi-arrow-down-up"
-                isActive={activeSort === "financial-result"}
-              />
-            </div>
-          </div>
-        </div>
-      </div>
+      {results.length > 0 && (
+        <CardsTotals
+          totalCompanies={totalCompanies}
+          totals={totals}
+          selectedYear={selectedYear}
+          selectedQuarter={selectedQuarter}
+          activeSort={activeSort}
+        />
+      )}
     </div>
   );
 }
---
+++ src/components/CardsTotals.jsx
@@ -0,0 +1,62 @@
+import { useTranslation } from "react-i18next";
+import DefinitionList from "./DefinitionList";
+import { parseDecimalNumber } from "../utils/decimalNumbers";
+
+export default function CardsTotals({
+  totalCompanies,
+  totals,
+  selectedYear,
+  selectedQuarter,
+  activeSort,
+}) {
+  const { t, i18n } = useTranslation();
+  const lang = i18n.language || "mk";
+
+  return (
+    <div className="sticky-bottom py-4">
+      <div className="container bg-total totals bg-opacity-25 backdrop-blur border border-light shadow-lg py-5 rounded">
+        <div className="row mx-2">
+          <div className="col-lg-6 col-xl-8 vstack gap-2 justify-content-center">
+            <h5 className="card-title">
+              {selectedQuarter > 0
+                ? t("cards.totalQuarter", {
+                    year: selectedYear,
+                    quarter: selectedQuarter,
+                  })
+                : t("cards.total", { year: selectedYear })}
+            </h5>
+            <p className="card-text mb-4 mb-lg-0">
+              {totalCompanies}{" "}
+              {totalCompanies % 10 === 1 && totalCompanies !== 11
+                ? t("cards.company_singular")
+                : t("cards.company_plural")}
+            </p>
+          </div>
+          <div className="col-lg-6 col-xl-4 align-self-center vstack gap-2 ps-xl-0">
+            <DefinitionList
+              title={t("cards.income")}
+              total={parseDecimalNumber(totals.income, lang)}
+              rawValue={totals.income}
+              icon="bi-arrow-down"
+              isActive={activeSort === "income"}
+            />
+            <DefinitionList
+              title={t("cards.expenses")}
+              total={parseDecimalNumber(totals.expenses, lang)}
+              rawValue={totals.expenses}
+              icon="bi-arrow-up"
+              isActive={activeSort === "expenses"}
+            />
+            <DefinitionList
+              title={t("cards.financial-result")}
+              total={parseDecimalNumber(totals["financial-result"], lang)}
+              rawValue={totals["financial-result"]}
+              icon="bi-arrow-down-up"
+              isActive={activeSort === "financial-result"}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
